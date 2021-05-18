1eb8320a5810cc508bfdd5e2fb6c6318ce783df1
氕氘氚 <cjp_ca@163.com>
2021-05-18 15:41:14 +0800

    add config

--- config.go
+++ config.go
@@ -5,5 +5,57 @@ import (
 )
 
 type Config struct {
-	AggerIntervalSec	int `toml:"aggrIntervalSec"`
+	AggrIntervalSec     int `toml:"aggrIntervalSec"`
+	Addr                string
+	Source              string
+	SendBatchSize       int
+	SendMaxIntervaSec   int
+	SendMaxRetryCount   int
+	SendTimeoutSec      int
+	MetricGCIntervalSec int
+	MetricGCSec         int
+}
+
+func (c *Config) ConfigValidate() error {
+	if c == nil {
+		return errors.New("config of tracer can't be nil")
+	}
+
+	if c.Addr == "" {
+		return errors.New("addr of tracer can't be nil")
+	}
+
+	if c.AggrIntervalSec == 0 {
+		c.AggrIntervalSec = 60
+	}
+
+	if c.Source == "" {
+		c.Source = "init etracer"
+	}
+
+	if c.SendBatchSize == 0 {
+		c.SendBatchSize = 1000
+	}
+
+	if c.SendMaxIntervaSec == 0 {
+		c.SendMaxIntervaSec = 10
+	}
+
+	if c.SendMaxRetryCount == 0 {
+		c.SendMaxRetryCount = 3
+	}
+
+	if c.SendTimeoutSec == 0 {
+		c.SendTimeoutSec = 5
+	}
+
+	if c.MetricGCSec == 0 {
+		c.MetricGCSec = 180 // 3min by default
+	}
+
+	if c.MetricGCIntervalSec == 0 {
+		c.MetricGCIntervalSec = 60 // 1min by default
+	}
+
+	return nil
 }
